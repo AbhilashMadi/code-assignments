6a2db4ba20d6a625fe207a101892299556d0a7d9
Abhilash Madi <madiabhilash1563@gmail.com>
2023-02-08 22:00:40 +0530

    setup

---
+++ src/components/cardPrint.jsx
@@ -0,0 +1,11 @@
+import "../css/printcard.css";
+import "../assets/print.svg";
+import "../assets/success.svg"
+
+import React from 'react'
+
+export const cardPrint = () => {
+  return (
+    <div><img src="" alt="" /></div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App";
+import "./css/index.css";
+import "./css/responsive.css";
+
+const rootElem = document.getElementById("root");
+const root = createRoot(rootElem);
+
+root.render(
+    <React.Fragment>
+        <App/>
+    </React.Fragment>
+)
